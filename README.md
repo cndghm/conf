c68b5bfce0b226c136e84210b64369bccdf053f2
Guilherme Augusto de Souza Candinho <cndghm@gmail.com>
2025-12-17 23:05:58 -0300

    Setting virtual terminal setup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Config Files
-Here's my config files.
+These are my configuration files for my virtual terminal setup.
+
+- Fonts: Terminus FreeBSD Size 32
+- Emacs: init.el file
+- Tmux: To be done
+- Irssi: To be done
--- init.el
+++ init.el
@@ -1,27 +1,21 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+(setq inhibit-startup-message t)
+(setq ring-bell-function 'ignore)
+(setq c-default-style "bsd")
+(setq c-basic-offset 8)
+(setq tab-width 8)
+(setq indent-tabs-mode t)
+(setq tty-escape-char ?\C-x)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq auto-save-default nil)
+(setq electric-indent-mode nil)
+(setq indent-line-function 'insert-tab)
 
-(ido-mode 1)
-(ido-everywhere 1)
-
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
+(show-paren-mode 1)
+(global-display-line-numbers-mode 1)
+(column-number-mode 1)
+(line-number-mode 1)
 (menu-bar-mode -1)
-(tooltip-mode -1)
-
-(column-number-mode)
-(global-display-line-numbers-mode t)
-
-(setq inhibit-splash-screen t)
-(setq initial-scratch-message ";; Happy Hacking")
-
-(setq backup-directory-alist `(("." . "~/.emacs.d/saves")))
-
-(load-theme 'monochrome t)
-
-(custom-set-variables
- '(package-selected-packages '(magit monochrome-theme rust-mode)))
-(custom-set-faces
- )
-
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(normal-erase-is-backspace-mode 1)
+(set-language-environment "UTF-8")
