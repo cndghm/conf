5e308f0b64a47e5448d638a83970444ef53fbc36
Guilherme Augusto de Souza Candinho <cndghm@gmail.com>
2025-12-20 07:58:38 -0300

    Added some packages
    Signed-off-by: Guilherme Augusto de Souza Candinho <cndghm@gmail.com>

--- init.el
+++ init.el
@@ -1,9 +1,17 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(ido-mode 1)
+(ido-everywhere 1)
+
 (setq inhibit-startup-message t)
 (setq ring-bell-function 'ignore)
 (setq c-default-style "bsd")
 (setq c-basic-offset 8)
 (setq tab-width 8)
 (setq indent-tabs-mode t)
+(setq tty-escape-char ?\C-x)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq auto-save-default nil)
 (setq electric-indent-mode nil)
@@ -16,4 +24,12 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(normal-erase-is-backspace-mode 1)
 (set-language-environment "UTF-8")
+
+(load-theme 'monochrome t)
+
+(custom-set-variables
+ '(package-selected-packages '(magit monochrome-theme)))
+(custom-set-faces
+ )
