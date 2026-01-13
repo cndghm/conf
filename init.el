6e4e4ba047c9b06a9beacf5db76705018810c37c
Guilherme Augusto de Souza Candinho <cndghm@gmail.com>
2026-01-13 00:37:12 -0300

    Updated init.el lean4-mode
    Signed-off-by: Guilherme Augusto de Souza Candinho <cndghm@gmail.com>

--- init.el
+++ init.el
@@ -2,16 +2,21 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+(add-to-list 'package-selected-packages 'dash)
+(add-to-list 'package-selected-packages 'lsp-mode)
+(add-to-list 'package-selected-packages 'magit-section)
+(add-to-list 'package-selected-packages 'cyberpunk-theme)
+(add-to-list 'load-path "~/.emacs.d/lean4")
+(require 'lean4-mode)
+
+(package-install-selected-packages 'no-confirm)
+
 (ido-mode 1)
 (ido-everywhere 1)
 
 (setq inhibit-startup-message t)
 (setq ring-bell-function 'ignore)
-(setq c-default-style "bsd")
-(setq c-basic-offset 8)
-(setq tab-width 8)
 (setq indent-tabs-mode t)
-(setq tty-escape-char ?\C-x)
 (setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 (setq auto-save-default nil)
 (setq electric-indent-mode nil)
@@ -27,10 +32,12 @@
 (normal-erase-is-backspace-mode 1)
 (set-language-environment "UTF-8")
 
-(set-face-attribute 'default nil :height 130)
-(load-theme 'monochrome t)
+(load-theme 'cyberpunk t)
 
 (custom-set-variables
- '(package-selected-packages '(magit monochrome-theme)))
+ '(custom-safe-themes
+   '("9fb69436c074b82a62b78b8d733e6274d0bd16d156f7b094e2afe4345c040c49"
+     default))
+ '(package-selected-packages '(cyberpunk-theme magit monochrome-theme)))
 (custom-set-faces
  )
