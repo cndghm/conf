887bad8b5d42e697c9856040f40b33ed93e92403
Guilherme Augusto de Souza Candinho <cndghm@gmail.com>
2026-01-18 13:42:59 -0300

    Updated some config files
    Signed-off-by: Guilherme Augusto de Souza Candinho <cndghm@gmail.com>

--- init.el
+++ init.el
@@ -2,15 +2,6 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(add-to-list 'package-selected-packages 'dash)
-(add-to-list 'package-selected-packages 'lsp-mode)
-(add-to-list 'package-selected-packages 'magit-section)
-(add-to-list 'package-selected-packages 'cyberpunk-theme)
-(add-to-list 'load-path "~/.emacs.d/lean4")
-(require 'lean4-mode)
-
-(package-install-selected-packages 'no-confirm)
-
 (ido-mode 1)
 (ido-everywhere 1)
 
@@ -32,12 +23,5 @@
 (normal-erase-is-backspace-mode 1)
 (set-language-environment "UTF-8")
 
-(load-theme 'cyberpunk t)
-
-(custom-set-variables
- '(custom-safe-themes
-   '("9fb69436c074b82a62b78b8d733e6274d0bd16d156f7b094e2afe4345c040c49"
-     default))
- '(package-selected-packages '(cyberpunk-theme magit monochrome-theme)))
-(custom-set-faces
- )
+;; Note: Install cyberpunk theme and magit
+;; (load-theme 'cyberpunk t)
