d2be3dd61bc12657b504e92db4e8b9f6c827bc8f
Guilherme Augusto de Souza Candinho <cndghm@gmail.com>
2026-01-23 00:35:20 -0300

    Updated config files
    Signed-off-by: Guilherme Augusto de Souza Candinho <cndghm@gmail.com>

--- init.el
+++ init.el
@@ -1,27 +1,27 @@
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-(ido-mode 1)
-(ido-everywhere 1)
-
 (setq inhibit-startup-message t)
-(setq ring-bell-function 'ignore)
-(setq indent-tabs-mode t)
-(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
-(setq auto-save-default nil)
-(setq electric-indent-mode nil)
-(setq indent-line-function 'insert-tab)
+(setq inhibit-startup-screen t)
+(setq inhibit-scratch-message nil)
 
-(show-paren-mode 1)
-(global-display-line-numbers-mode 1)
-(column-number-mode 1)
-(line-number-mode 1)
+(set-language-environment "UTF-8")
+(set-default-coding-systems 'utf-8)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(normal-erase-is-backspace-mode 1)
-(set-language-environment "UTF-8")
 
-;; Note: Install cyberpunk theme and magit
-;; (load-theme 'cyberpunk t)
+(line-number-mode t)
+(column-number-mode t)
+(global-display-line-numbers-mode 1)
+
+(setq-default ident-tabs-mode t)
+(setq-default tab-width 8)
+
+(setq c-default-style "bsd")
+(setq c-basic-offset 8)
+
+(setq make-backup-files nil)
+(setq auto-save-defaults nil)
+
+(show-paren-mode 1)
+
+(normal-erase-is-backspace-mode 1)
--- profile
+++ profile
@@ -1,15 +1,8 @@
-PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:~/bin
-export PATH
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin:$HOME/bin; export PATH
 
-TERM=${TERM:-xterm}
-export TERM
-
-PAGER=less
-export PAGER
-
-EDITOR=emacs
-export EDITOR
+EDITOR=vi;   	export EDITOR
+PAGER=less;  	export PAGER
 
 ENV=$HOME/.shrc; export ENV
 
-export XKB_DEFAULT_LAYOUT="br"
+if [ "$PWD" != "$HOME" ] && [ "$PWD" -ef "$HOME" ] ; then cd ; fi
